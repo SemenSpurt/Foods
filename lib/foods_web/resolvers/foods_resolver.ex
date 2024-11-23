7996c7234c0a828e0e933a29291118b106f87386
SemenSpurt <spurtsemen@gmail.com>
2024-11-23 17:35:44 +0300

    qurey and mutation

--- lib/foods_web/resolvers/foods_resolver.ex
+++ lib/foods_web/resolvers/foods_resolver.ex
@@ -4,4 +4,25 @@ defmodule FoodsWeb.FoodsResolver do
     def all_products(_root, _args, _info) do
       {:ok, Food.list_products()}
     end
+
+    def create_product(_root, args, _info) do
+      case Food.create_product(args) do
+        {:ok, product} ->
+          {:ok, product}
+        _error ->
+          {:error, "could not create product"}
+      end
+    end
+
+
+
+    # def delete_product(_root, args, _info) do
+    #   product = Food.get_product!(args)
+    #   case Food.delete_product(product) do
+    #     {:ok} ->
+    #       {:ok, "product has been deleted"}
+    #     _error ->
+    #       {:error, "could not create product"}
+    #   end
+    # end
   end
--- lib/foods_web/schema.ex
+++ lib/foods_web/schema.ex
@@ -17,4 +17,24 @@ defmodule FoodsWeb.Schema do
         resolve(&FoodsResolver.all_products/3)
       end
     end
-  end
+
+    mutation do
+      @desc "Create a new link"
+      field :create_product, :product do
+        arg :name, non_null(:string)
+        arg :prots, non_null(:float)
+        arg :fats, non_null(:float)
+        arg :hydro, non_null(:float)
+      
+        resolve &FoodsResolver.create_product/3
+      end
+
+    #   @desc "Create a new link"
+    #   field :delete_product, :product do
+    #     arg :id, non_null(:integer)
+      
+    #     resolve &FoodsResolver.delete_product/3
+    #   end
+    end
+
+end
